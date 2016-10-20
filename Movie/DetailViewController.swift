174722bbb1e42c337dd1f5826ad139cf2bbbb7b4
Shitianyu Pan <pan.210@buckeyemail.osu.edu>
2016-10-20 00:10:36 -0500

    Ready for Demo

--- Movie/DetailViewController.swift
+++ Movie/DetailViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import Social
 
 class DetailViewController: UIViewController {
     @IBOutlet weak var rating: UILabel!
+    @IBOutlet weak var poster: UIImageView!
+    
+    @IBOutlet weak var director: UILabel!
+    @IBOutlet weak var releaseDate: UILabel!
+    @IBOutlet weak var runningTime: UILabel!
+    
+    var favortieViewController : FavoriteViewController?=nil
+    var movieTitle: String!
+    
     var detailItem: Movie? {
         didSet {
             // Update the view.
@@ -20,15 +30,76 @@ class DetailViewController: UIViewController {
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = self.detailItem {
-            if let label = self.rating {
-                label.text = detail.year
+            self.movieTitle = detail.title
+            updateDetail(detail.id)
+            if detail.posterPath != "" {
+                if let imageView = self.poster{
+                    imageView.image = detail.poster
+                }
             }
+        }
+    }
+    
+    func updateDetail(id: String){
+        let url = NSURL(string:"http://www.omdbapi.com/?plot=short&r=json&i=" + id)!
+        let json = JSON(data: NSData(contentsOfURL: url)!)
+        
+        if let error = json["Error"].string{
+            //Movie not found
+            print(error)
+        } else {
+            let release = String(json["Released"])
+            let runtime = String(json["Runtime"])
+            let director = String(json["Director"])
+            let rating = String(json["imdbRating"])
+            
+            if let label = self.rating {
+                label.text = "Rating: " + rating
+            }
+            
+            if let label = self.runningTime {
+                label.text = "Running Time: " + runtime
+            }
+            
+            if let label = self.releaseDate {
+                label.text = "Release Date: " + release
+            }
+            
+            if let label = self.director {
+                label.text = "Director: " + director
+            }
+        }
+    
+    }
+    
+    @IBAction func addToFavorites(sender: UIButton) {
+        self.favortieViewController?.insertNewObject(self.movieTitle)
+    }
+    
+    /**
+     copy from https://www.codementor.io/swift/tutorial/ios-development-facebook-twitter-sharing
+     */
+    @IBAction func shareOnFacebook(sender: UIButton) {
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook) {
+            let fbShare = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+            
+            self.presentViewController(fbShare, animated: true, completion: nil)
+            
+        } else {
+            let alert = UIAlertController(title: "Accounts", message: "Please login to a Facebook account to share.", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let tabBar = self.tabBarController {
+            let controllers = tabBar.viewControllers
+            self.favortieViewController = controllers![controllers!.count-1] as? FavoriteViewController
+        }
         self.configureView()
     }
     
--- Movie/FirstViewController.swift
+++ Movie/FirstViewController.swift
@@ -14,10 +14,19 @@ class FirstViewController: UIViewController, UISearchBarDelegate, UICollectionVi
     
     @IBOutlet weak var movieView: UICollectionView!
     @IBOutlet weak var searchBar: UISearchBar!
+    
+    var activityIndicator: UIActivityIndicatorView!
     var movies = [Movie]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+        activityIndicator.hidden = true
+        self.view.addSubview(activityIndicator)
         // Do any additional setup after loading the view, typically from a nib.
 //        if let split = self.splitViewController {
 //            let controllers = split.viewControllers
@@ -34,7 +43,6 @@ class FirstViewController: UIViewController, UISearchBarDelegate, UICollectionVi
         if segue.identifier == "showDetail" {
             if let indexPath = self.movieView.indexPathForCell(sender as! UICollectionViewCell){
                 let movie = movies[indexPath.row]
-                print(movie)
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = movie
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
@@ -88,36 +96,32 @@ class FirstViewController: UIViewController, UISearchBarDelegate, UICollectionVi
             return
         }
         self.movies.removeAll()
-        
+        activityIndicator.hidden = true
+        activityIndicator.startAnimating()
+        dispatch_async(dispatch_get_main_queue(), {
+            self.movieView.reloadData()
+        })
         var text = searchBar.text!
         text = text.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
         
-        let url = NSURL(string:"http://www.omdbapi.com/?r=json&s="+text)!
+        let url = NSURL(string:"http://www.omdbapi.com/?r=json&page=1&s="+text)!
       
         let json = JSON(data: NSData(contentsOfURL: url)!)
         
         if let error = json["Error"].string{
             //Movie not found
-            print(error)
-        } else {
-            let movieInfos = json["Search"]
-            var moviesWithPoster = [Movie]()
+            self.activityIndicator.stopAnimating()
+            let alert = UIAlertController(title: error, message: "", preferredStyle: UIAlertControllerStyle.Alert)
             
-            for index in 0...movieInfos.count-1 {
-                let title = String(movieInfos[index]["Title"])
-                let id = String(movieInfos[index]["imdbID"])
-                let year = String(movieInfos[index]["Year"])
-                let posterPath = String(movieInfos[index]["Poster"])
-                let type = String(movieInfos[index]["Type"])
-                if posterPath == "N/A" {
-                    let movie = Movie(id: id, title: title, posterPath: "", type: type, year: year)
-                    self.movies.append(movie)
-                    
-                } else {
-                    let movie = Movie(id: id, title: title, posterPath: posterPath, type: type, year: year)
-                    moviesWithPoster.append(movie)
-                }
-            }
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) in
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }))
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+        } else {
+            
+            var moviesWithPoster = fetchHelper(json["Search"])
+            print(moviesWithPoster.count)
             
             dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), {
                 for i in 0...moviesWithPoster.count-1 {
@@ -128,11 +132,35 @@ class FirstViewController: UIViewController, UISearchBarDelegate, UICollectionVi
                 self.movies.insertContentsOf(moviesWithPoster, at: 0)
                 dispatch_async(dispatch_get_main_queue(), {
                     self.movieView.reloadData()
+                    self.activityIndicator.stopAnimating()
                 })
             })
         }
         
         
+    }
+    
+    func fetchHelper(movieInfos: JSON) -> [Movie]{
+        var moviesWithPoster = [Movie]()
+        
+        for index in 0...movieInfos.count-1 {
+            let title = String(movieInfos[index]["Title"])
+            let id = String(movieInfos[index]["imdbID"])
+            let year = String(movieInfos[index]["Year"])
+            let posterPath = String(movieInfos[index]["Poster"])
+            let type = String(movieInfos[index]["Type"])
+            if posterPath == "N/A" {
+                let movie = Movie(id: id, title: title, posterPath: "", type: type, year: year)
+                self.movies.append(movie)
+                
+            } else {
+                let movie = Movie(id: id, title: title, posterPath: posterPath, type: type, year: year)
+                moviesWithPoster.append(movie)
+            }
+        }
+        
+        return moviesWithPoster
+
     }
 }
 
